d33498303f358f55d0da32ca44f589e74dbd7910
Aliya Rahman <aliya.rahman@yahoo.com>
2014-04-28 06:57:34 -0400

    Added trivia game

---
+++ app/static/FellowTrivia.py
@@ -0,0 +1,394 @@
+while 1:
+#Paulino ========================================================================================
+#================================================================================================
+	print "\nMariella's trivia: Welcome to the Sound of Silence"
+	print "\n\n1.BLANK in 10 Americans have hearing loss"
+	print "\n(a)Two in Ten \nb)Five in Ten  \n(c)Three in ten"
+	#This part will ask you to pick one of the aptions from above and choose an answer
+	answer = raw_input ("Your answer:  ")
+	#THis part will let you know how your soul fared in knowing the Mighty Marie 
+	if answer=="a":
+		print "Yes, That is correct, two in ten Americans suffer from hearing loss"
+	else: 
+		print "Unfortunetely, you may have overestimated, the stats are two in ten.  This is still very significant and with a large impact"
+
+	#--------------------Section 3 - Second Question 
+	#This part will ask you if you know Mariella's sister name?
+
+	print "\n2. What percentage of the 48 million Americans in the United States suffer from hearing loss?"
+	print "\n(a)5% \nb)10%  \n(c)20%"
+
+
+	#This part will ask you to pick one of the aptions from above and choose an answer
+	answer = raw_input ("Your answer:  ")
+
+	#THis part will let you know how your soul fared in knowing the Mighty Marie 
+	if answer=="c":
+		print "Indeed, 20 percent of Americans suffer from hearing loss, with the number increasing every year!"
+
+	else: 
+		print "Unfortunately the number is much higher and over 20 percent of Americans suffer from hearing loss, with the number increasing every year!"
+
+#Kamlley ========================================================================================
+#================================================================================================
+	print "\n\nElaine Kamlley says: Let's spend some Queer Time!"
+	print "-------------------"
+
+
+	#Section 2. The first question
+	print "\n 1. What year did Audre Lorde release Zami?"
+	print "\n (a) 1990 (b) 1975 (c) 1982"
+
+	answer = raw_input("\n Your answer: ")
+
+	#Section 3. Answer
+	#Checks to see if answer is correct. If it is, say that's correct! If it
+	#if it isn't, say the correct answer
+
+	if answer == "c":
+		print "yes! It is also the year Elaine was born. \nMaking it the best year ever."
+	else:
+		print "Sorry, but its (c) 1982. It is also the year Elaine was born."
+
+	#Section 4. The second question
+	print "\n 2. What year did ACT UP form?"
+	print "\n (a) 1992 (b) 1987 (c) 1980"
+
+	answer = raw_input("Your answer: ")
+
+	#Section 5. Answer for second question
+	#Checks to see if answer is correct. If it is, say that's correct! If it
+	#if it isn't, say the correct answer
+	if answer == "b":
+		print "That's Right!"
+	else: 
+		print "Sorry but its (b) 1987"
+
+
+
+#Smith ========================================================================================
+#================================================================================================
+
+	print "\n\nBobby Joe says: Welcome to the game, prepare to test your wits!"
+
+	#Section 1. The first question
+	print "\nAccording to the Center for American Progress, what proportion of US citizens will live at least one year of their lives in poverty?"
+	print "\ta) One-fifth \n\tb) One-third \n\tc) One-half"
+	answer = raw_input("Your answer: ")
+
+	if answer == "b":
+		print "Beginners luck!\n"
+	else:
+		print 'Actually, the answer is "b". One-third of US citizens will live at least one year of their lives in poverty.\n'
+
+
+	#Section 2. The second question
+	print "According to the Children's Defense Fund (CDF; 2010), how often is a child born into poverty in the US?"
+	print "\ta) Every 32 seconds \n\tb) Every 3 minutes and 2 seconds \n\tc) Every 32 minutes"
+	answer = raw_input("Your answer: ")
+
+	if answer == "a":
+		print "Correct!\n"
+	else:
+		print 'Sorry! The answer is "a". A child is born into poverty in the US every 32 seconds \n'
+
+	#Section 3. The third question
+	print "Where do most poor people in the US live?"
+	print "\ta) In inner cities \n\tb) Outside of inner cities"
+	answer = raw_input("Your answer:")
+
+	if answer == "b":
+		print "Correct!\n"
+	else:
+		print 'Good guess, but the correct answer is "b". Most poor people in the US live outside of inner cities.\n'
+
+#Davis ====================================================
+#==========================================================
+	print "\n\n Pam's trivia on Walmart and the economy!"
+	print "\n2. What does it cost Taxpayers each year to keep a Walmart SuperCenter open in your City?  "
+	print "\n (a) $2M \t (b) $900K \t  (c) $100K"
+
+	answer = raw_input("Your answer: ") #Asks the user for their reponse and saves it as answer
+	if answer=="b": #Checks to see if answer is correct.  If it is, say that's Correct! If it isn't, say so.
+		print "Right!"
+	else:
+		print "Wrong! The answer is approximately $900k.\n"
+
+		
+	print "3. What is the percentage of 1.3 million Walmart Associates that qualify or are eligible to for Government Assistance? "
+	print "\n (a) 3 \t (b) 25 \t  (c) 80"
+
+	answer = raw_input("Your answer: ") #Asks the user for their reponse and saves it as answer
+	if answer=="c": #Checks to see if answer is correct.  If it is, say that's Correct! If it isn't, say so.
+		print "Right!"
+	else:
+		print "Wrong! The right answer is 80 percent \n"
+
+
+
+#Musuta============================================================================
+#=============================================================================
+
+	print "\n Selina's trivia:"
+	print "\nHow many D.C. residents benefited from the SNAP program in 2011? "
+	print "a) 30,000 \nb) 250,000 \nc) 135,000"
+
+	#Ask the user for their response
+	answer = raw_input("Your answer: ")
+
+	#If the user inputs the letter c, then print "You got it! Almost a 1/4 of DC's residents have benefited from the SNAP program"
+
+	if answer == "c":
+		print "You got it! Almost a 1/4 of DC's residents have benefited from the SNAP program"
+
+	elif answer == "b":
+		print "The answer is c) but you are close."
+
+	else:
+		print "Unfortunately, the need is great. The answer is c). A 1/4 od DC's residents benefited from the SNAP Program in 2011."
+
+	# Section 2. second question
+
+	print "\nWhat is the SNAP Expansion Act?"
+	print "a) Expands eligibility of low-income residents \nb) Increases benefits for SNAP recipients \nc) both"
+
+	answer = raw_input("Your answer: ")
+
+	if answer == "c":
+		print "Right on!"
+
+	else:
+		print "\nThe answer is c). The District of Columbia passed this piece of legislation 2009 to expand eligibility and raise benefits for SNAP recipients"
+
+
+
+#====Henderson
+#===================================
+
+	#Section 1. The first question
+	print "\n\nCasidy Henderson's trivia on transgender lives"
+	print "\n1. What is the transgender suicide rate?"
+	print "\n(a) 1% (b) 36% (c) 34% (d) 49% "
+
+	answer = raw_input("\nwhat do you think is the case?") 
+
+	if answer== "d": 
+		print "good job, your well informed "
+	else :
+
+		print "Nope! It's D."
+
+
+
+	#Section 2. The Second question
+	print "\n\n2. What is the national average percentage of transgender people that are homeless?"
+	print "\n(a) 81% (b) 18% (c) 14% (d) 74% "
+
+	answer = raw_input("\nwhat do you think is the case?") 
+
+	if answer== "b": 
+		print "good job, your well informed "
+	else :
+
+		print "Nope! It's b."
+
+
+#========Martinez
+#======================
+
+#Section 1. The first questions
+
+	print "\n Aurea's trivia script!"
+	print "\n 1. In what county is the city of Costa Mesa located in?"
+	print "a)Los Angeles b)Orange c)San Diego d)Santa Barbara"
+	answer=raw_input ("\n Your answer: ") #Asks the user for their response and save it as answer
+
+	if answer=="b":  #Checks to see if answer is correct. If it is, say that's correct! If it isn't, say so.
+		print "You are a smart cookie!"
+	else:
+		print "Nope.Nice try but Costa Mesa is located in Orange County."
+
+	#Section 2. The second question
+
+	print "\n 2. At what age did Aurea immigrate to the United States?"
+	print "a)8 b)6 c)4 d)2"
+	answer = raw_input ("\n Your answer: ") 
+
+	if answer== "d":
+		print "Wow you know my life!"
+	else:
+		print "Your a few years off. I was a 2 year old baby."
+
+
+#==== Ortiz
+#========================================
+	#Section 1. First question
+	print "Kathy's Trivia: Are you from Red Hook?"
+	print "\n1. What borough is Red Hook in?"
+	print "\n(a)Washington  (b)Queens  (c)Brooklyn (d)Bronx"
+
+
+	answer = raw_input("Your answer: ")
+
+	if answer=="c":
+		print "Damn Right!!"
+
+	else:
+		print "Your Dead Wrong"	
+
+
+
+		#Section 2. Second question
+	print "\n2. What movie director made a really bad movie about Red Hook?"
+	print "\n(a)Michael Bay  (b)Spike Lee  (c)Quintin Tarrantino (d)Tim Burrton"
+
+
+	answer = raw_input("Your answer: ")
+
+	if answer=="b":
+		print "Damn Right!!"
+
+	else:
+		print "Your Dead Wrong"	
+
+
+		#Section 3. Third question
+	print "\n3. What famous artist made a music video in Red Hook last year?"
+	print "\n(a)Lorde  (b)50 Cent  (c)Mick Jagger (d)Beyonce"
+
+
+	answer = raw_input("Your answer: ")
+
+	if answer=="a":
+		print "Damn Right!!"
+
+	else:
+		print "Your Dead Wrong"	
+
+
+
+		#Section 4. Forth question
+	print "\n4. What TV show is constantly in Red Hook?"
+	print "\n(a)Vampire Diaries  (b)Silicon Valley (c)Brooklyn 99 (d)Law and Order"
+
+
+	answer = raw_input("Your answer: ")
+
+	if answer=="d":
+		print "Damn Right!!"
+
+	else:
+		print "Your Dead Wrong"	
+
+
+
+		#Section 5. Fith question
+	print "\n5. What big market came to Red Hook almost 5 years ago?"
+	print "\n(a)Fairway (b)Home Depot  (c)Ikea (d)Lowes"
+
+
+	answer = raw_input("Your answer: ")
+
+	if answer=="c":
+		print "Damn Right!!"
+
+	else:
+		print "Your Dead Wrong"	
+
+
+#Towns ===========================================
+#========================================================
+	print "\n\nJason's trivia!"
+	print "\nDid a black inventor create the original Super Soaker water gun?"
+	print  "\n(a) no way (b) yes (c) I have absolutely no idea"
+
+	answer = raw_input("Your answer:")
+
+	if answer =="b":
+		print "Trueee! Lonnie George Johnson, a black inventor and NASA engineer, created the Super Soaker. It was the top selling toy in the US in 1991 and 1992."
+	else:
+		print "Nope! Sorry, but you were wrong here. Lonnie George Johnson, a black inventor, created the Super Soaker. It was the top selling toy in the US in 1991 and 1992.\n"
+		
+
+
+#=======Acker
+#==========================
+
+	print "\n\nTerri's questions!"
+	print "\n1. What's the name of an area of a city where it is difficult to find affordable fresh food?"
+	print "\n(a) hungryville (b) food desert (c) International food store"
+	if answer =='b':
+		print "Correct!"
+	else:
+		print "Wrong!\n"
+
+
+
+#======Rollins
+#================
+	print "\n\nMe and You, Organizers, and yo momma too. \n Name that really dope Black Radical Feminist Organizer!"
+
+	# Section 1. The First Question
+	print "She said: Strong People don't need strong leaders."
+	print "\n(a) Audre Lorde (b) Angela Davis (c) Ella Baker (d) Fannie Lou Hamer"
+
+	answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+	if answer=="c": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+		print "She did that!"
+	else: print "So what you're saying is you're not a feminist...\n"
+
+
+	# Section 2. The Second Question
+	print "\n\nShe said: It is our duty to fight for our freedom. It is our duty to win.\nWe must love each other and protect each other. We have nothing to lose but our chains."
+	print "\n(a) Assata Shakur (b) Barbera Ransby (c) Cecile Fatiman (d) Elaine Brown"
+
+	answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+	if answer=="a": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+		print "She said that!"
+	else: print "So now you dont believe in self-care and community accountability...interesting.\n"
+
+	# Section 3. The Third Question
+	print "\n\nLoss of social standing is an ever-present threat for individuals whose social acceptance is based on behavioral traits rather than unconditional human value."
+	print "\n(a) Beyonce (b) Nikki Giovanni (c) Chimimanda Ngozi Adichie (d) Melissa Harris Perry"
+
+	answer = raw_input("Your answer:") #Asks the user for their response and saves it as answer
+
+	if answer=="d": #Checks to see if answer is correct. If it is, say thats correct! If it isn't, say so.
+		print "She surely said that!"
+	else: print "Youve gotta do better.\n"
+
+#=====Bailon
+#====================
+
+	print "\n\nDago says: Welcome to the UndocuQueer Game"
+
+	print "\n1. Out of the 11million Undocumented \n people how many are estimated to be LGBT-Identified?"
+	print "\n (a) 36,000  (b)11million (c)267,000 "
+
+	answer = raw_input("Your answer: ")
+
+	if answer=="c":
+		print "Correct, You're Fabulous!"
+	else: 
+		print "Not quite, currently there are 267,000 LGBT-Identified Undocumented people \n This is not counting people who are not out and kids!"
+
+	print "\n\n2. How many Binational couples are there in the U.S.?"
+	print "\n (a) 15,000  (b) 24,700  (c) 32,300 "
+
+	anwer = raw_input("Your answer: ")
+	if answer=="b":
+		print "Correct, You are on fire!"
+	else:
+		print "Hmmm...not really there are about 24,7000 Binational couples in the U.S!"
+
+
+	print "\n\n3. What part of the Defense of Marrige Act, get repealed? "
+	print "\n (a) all of it  (b)section 2 (c)Section 3 "
+
+	answer = raw_input("Your answer: ")
+	if answer=="c":
+		print "Correct, You're Fierce!"
+	else: 
+		print "This is a common mistake, Actually Section 3 of DOMA was struck down this grants federal rights to same-sex couples and leaves it to\n the states to decide whether or not they recognize same-sex marriage"
