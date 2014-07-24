5f9ce9628160bd6b011706cea3e0f17ecb85b938
aliyarahman <aliya.rahman@yahoo.com>
2014-07-24 16:09:20 -0400

    Corrected numbering in csv

--- import_badge.py
+++ import_badge.py
@@ -1,8 +1,17 @@
+# Draws on a MySQL database using Flask-SQLAlchemy
 import csv
 from app import db, models
 
+
+# Clears current badges
+current_badges = models.Achievement.query.all()
+for c in current_badges:
+  db.session.delete(c)
+  db.session.commit()
+
 counter = 0
 
+#Adds new badges
 with open('badges.csv', 'rb') as csvfile:
 	badges = csv.reader(csvfile, delimiter=',', quotechar='"')
 	for row in badges:
